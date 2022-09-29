e917a661bc02a7edc08903bc72c1e7260de21cb0
hakanzn <hakanzn@gmail.com>
2022-09-29 03:56:03 +0300

    Add files via upload

--- js/hakan.js
+++ js/hakan.js
@@ -1,30 +1,26 @@
 var scene;
 var camera;
 var renderer;
-var url;
-if(document.URL.search("url=")>0){
-       url = document.URL.split("url=")[1];
-    }
-    else{
-       url= "https://hakanzn.github.io/kuredunya/textures/vahdet.jpeg";
-    }
+var spotlight;
 function createScene(){
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight,1, 1000);
-    camera.position.x = 15;
-    camera.position.y = 40;
-    camera.position.z = 40;
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight,1, 10000);
+    camera.position.x = 1;
+    camera.position.y = 1000;
+    camera.position.z = 1;
     camera.lookAt(new THREE.Vector3(0,0,0));
 
 
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({physicallCorrectLights:true, antialias:true, powerPreference:"high-performance"});
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.renderReverseSided = false;
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.getElementById("webgl").appendChild(renderer.domElement);
 
-    var ambientlight = new THREE.AmbientLight(0x404040, 3);
+    var ambientlight = new THREE.AmbientLight(0x404040, 2);
     scene.add(ambientlight);
 
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -33,7 +29,11 @@ function createScene(){
 
 
 
-    createBox("box1", 30,32,32,1, 1, 1, 0xfffff);
+    createBox("box1", 20,20,20,1, 100, 1, 0xffffff, "floor");
+    createBox("box2", 20,20,20,1, 200, 1, 0xffffff, "gunes");
+    createPlane("plane1", 1000,1000);
+    createSpotLight();
+    milkway();
 
     render();
 
@@ -41,21 +41,79 @@ function createScene(){
 
 function render(){
     renderer.render(scene, camera);
-    //scene.getObjectByName("box1").rotation.x += 0.01;
+    date = Date.now() * 0.001;
+    date2 = Date.now() * 0.0011;
+    orbitRadius = 200;
+    scene.getObjectByName("box1").position.set(
+  Math.cos(date) * orbitRadius,
+  200,
+  Math.sin(date) * orbitRadius
+);
+scene.getObjectByName("box2").position.set(
+  Math.cos(date2) * 250,
+  200,
+  Math.sin(date2) * 250
+);
+
+x = scene.getObjectByName("box2").position.x;
+    y = scene.getObjectByName("box2").position.y;
+    z = scene.getObjectByName("box2").position.z;
+    spotLight.position.set(x,y,z);
+
+
+
     //scene.getObjectByName("box1").rotation.y += 0.01;
     requestAnimationFrame(render);
 }
-function createBox(name, r, hs, ws, w,h,d,color){
+function createBox(name, r, hs, ws, w,h,d,color, texture){
     var geometry = new THREE.SphereGeometry(r, hs, ws);
-    var material = new THREE.MeshBasicMaterial({color:color});
+    var material = new THREE.MeshStandardMaterial({color:color});
     var loader = new THREE.TextureLoader();
-    
-
-    material.map = loader.load(url);
+    material.map = loader.load("../textures/"+texture+".jpg");
     var mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(w,h,d);
+
     mesh.name = name;
     scene.add(mesh);
+
+}
+
+function createPlane(name, w,h){
+    var geometry = new THREE.PlaneGeometry(w,h);
+    var material = new THREE.MeshStandardMaterial({color:0xffffff});
+    var loader = new THREE.TextureLoader();
+    material.map = loader.load("../textures/duz.jpg");
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(1,1,1);
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.name = name;
+    //mesh.rotateX( Math.PI / 2 );
+
+    scene.add(mesh);
+
+}
+
+function createSpotLight(){
+    spotLight = new THREE.PointLight(0xffffff,4,400);
+
+    spotLight.position.set(1,1000,1);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
+}
+
+function milkway(){
+    const loader = new THREE.CubeTextureLoader();
+loader.setPath( '../textures/MilkyWay/' );
+
+const textureCube = loader.load( [
+	'dark-s_px.jpg', 'dark-s_nx.jpg',
+	'dark-s_py.jpg', 'dark-s_ny.jpg',
+	'dark-s_pz.jpg', 'dark-s_nz.jpg'
+] );
+
+scene.background=textureCube;
+scene.add(textureCube);
 
 }
 
