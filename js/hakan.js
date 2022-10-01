36293ae7f8a918b0dff643b7c2fa1427babdd612
hakanzn <hakanzn@gmail.com>
2022-10-01 04:06:16 +0300

    Update hakan.js

--- js/hakan.js
+++ js/hakan.js
@@ -2,6 +2,7 @@ var scene;
 var camera;
 var renderer;
 var spotlight;
+var camera2;
 function createScene(){
 
     scene = new THREE.Scene();
@@ -11,11 +12,37 @@ function createScene(){
     camera.position.z = 1;
     camera.lookAt(new THREE.Vector3(0,0,0));
 
+    //ikinci kamera
+
+    camera2 = new THREE.PerspectiveCamera(10, window.innerWidth/window.innerHeight,1, 200);
+    camera2.position.x = 80;
+    camera2.position.y = 1;
+    camera2.position.z = 120;
+
+
+    camera2.lookAt(new THREE.Vector3(0,0,0));
+
+    var cameraHelper = new THREE.CameraHelper(camera2);
+    scene.add(cameraHelper);
+
+    //3.kamera
+
+    camera3 = new THREE.PerspectiveCamera(10, window.innerWidth/window.innerHeight,1, 200);
+    camera3.position.x = 200;
+    camera3.position.y = 1;
+    camera3.position.z = -200;
+
+
+    camera3.lookAt(new THREE.Vector3(0,0,0));
+
+    cameraHelper2 = new THREE.CameraHelper(camera3);
+    scene.add(cameraHelper2);
+
 
 
     renderer = new THREE.WebGLRenderer({physicallCorrectLights:true, antialias:true, powerPreference:"high-performance"});
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.renderReverseSided = false;
+    //renderer.shadowMap.renderReverseSided = false;
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.getElementById("webgl").appendChild(renderer.domElement);
@@ -24,16 +51,22 @@ function createScene(){
     scene.add(ambientlight);
 
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
     controls.update();
 
 
 
 
-    createBox("box1", 20,20,20,1, 100, 1, 0xffffff, "floor");
-    createBox("box2", 20,20,20,1, 200, 1, 0xffffff, "gunes");
+    createBox("box1", 20,20,20,1, 100, 1, 0xffffff, "floor", false, 0);
+    createBox("box2", 20,20,20,1, 200, 1, 0xffffff, "gunes", false, 0);
+    //createBox("stars", 500,100,100,1,1,1, 0xaaaa00, "stars", true, 0.1);
     createPlane("plane1", 1000,1000);
     createSpotLight();
     milkway();
+
+    //ızgara
+    //gridHelper = new THREE.GridHelper(10,100);
+    //scene.add(gridHelper);
 
     render();
 
@@ -60,16 +93,25 @@ x = scene.getObjectByName("box2").position.x;
     z = scene.getObjectByName("box2").position.z;
     spotLight.position.set(x,y,z);
 
+    //camera2 position
+    camera2.lookAt(new THREE.Vector3(Math.cos(date) * orbitRadius,
+  200,
+  Math.sin(date) * orbitRadius));
+
+    camera3.lookAt(new THREE.Vector3(Math.cos(date) * orbitRadius,
+  200,
+  Math.sin(date) * orbitRadius));
+
 
 
     //scene.getObjectByName("box1").rotation.y += 0.01;
     requestAnimationFrame(render);
 }
-function createBox(name, r, hs, ws, w,h,d,color, texture){
+function createBox(name, r, hs, ws, w,h,d,color, texture, bool, deger){
     var geometry = new THREE.SphereGeometry(r, hs, ws);
-    var material = new THREE.MeshStandardMaterial({color:color});
+    var material = new THREE.MeshStandardMaterial({color:color, transparent:bool, opacity:deger});
     var loader = new THREE.TextureLoader();
-    material.map = loader.load("https://hakanzn.github.io/kuredunya/textures/"+texture+".jpg");
+    material.map = loader.load("../textures/"+texture+".jpg");
     var mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(w,h,d);
 
@@ -82,7 +124,7 @@ function createPlane(name, w,h){
     var geometry = new THREE.PlaneGeometry(w,h);
     var material = new THREE.MeshStandardMaterial({color:0xffffff});
     var loader = new THREE.TextureLoader();
-    material.map = loader.load("https://hakanzn.github.io/kuredunya/textures/duz.jpg");
+    material.map = loader.load("../textures/duz.jpg");
     var mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(1,1,1);
     mesh.rotation.x = -Math.PI / 2;
@@ -104,7 +146,7 @@ function createSpotLight(){
 
 function milkway(){
     const loader = new THREE.CubeTextureLoader();
-loader.setPath( 'https://hakanzn.github.io/kuredunya/textures/MilkyWay/' );
+loader.setPath( '../textures/MilkyWay/' );
 
 const textureCube = loader.load( [
 	'dark-s_px.jpg', 'dark-s_nx.jpg',
