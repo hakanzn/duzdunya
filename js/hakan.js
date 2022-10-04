d30ff0e751655203f7957eb4e223aba50021e1d1
hakanzn <hakanzn@gmail.com>
2022-10-04 08:42:06 +0300

    Update hakan.js

--- js/hakan.js
+++ js/hakan.js
@@ -9,8 +9,8 @@ function createScene(){
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight,1, 10000);
     camera.position.x = 1;
-    camera.position.y = 1000;
-    camera.position.z = 1;
+    camera.position.y = 800;
+    camera.position.z = 400;
     camera.lookAt(new THREE.Vector3(0,0,0));
 
     //ikinci kamera
